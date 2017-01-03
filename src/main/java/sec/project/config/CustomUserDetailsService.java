88675a7bf257194bd8eb9d73435582c612f2ed6f
Kaisa Saren <kaisa.saren@helsinki.fi>
2017-01-03 08:18:44 +0200

    Connecting to sample database

--- src/main/java/sec/project/config/CustomUserDetailsService.java
+++ src/main/java/sec/project/config/CustomUserDetailsService.java
@@ -1,5 +1,13 @@
 package sec.project.config;
 
+//omat importit
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import org.h2.tools.RunScript;
+
 import java.util.Arrays;
 import java.util.Map;
 import java.util.TreeMap;
@@ -17,20 +25,43 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @PostConstruct
     public void init() {
-        // this data would typically be retrieved from a database
-        this.accountDetails = new TreeMap<>();
-        this.accountDetails.put("ted", "$2a$06$rtacOjuBuSlhnqMO2GKxW.Bs8J6KI0kYjw/gtF0bfErYgFyNTZRDm");
+        // Open connection to a database
+        String databaseAddress = "jdbc:h2:file:./database";
+        Connection connection;
+        
+        try {
+            //connection without user details
+            connection = DriverManager.getConnection(databaseAddress);
+            
+            //connect the MySql sample database to connection instance
+            RunScript.execute(connection, new FileReader("sql/mysqlsampledatabase.sql"));
+            
+            this.accountDetails = new TreeMap<>();
+            ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM customers");
+            System.out.println("Customers in database:"); //test
+            while (resultSet.next()) {
+                String name = resultSet.getString("customerName");
+                String address = resultSet.getString("addressLine1");
+                accountDetails.put(name, address);
+                
+                System.out.println(name + "\t" + address);  //test
+            }
+
+        } catch (Throwable t) {
+            System.err.println(t.getMessage());
+        }
+        
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if (!this.accountDetails.containsKey(username)) {
-            throw new UsernameNotFoundException("No such user: " + username);
+    public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
+        if (!this.accountDetails.containsKey(name)) {
+            throw new UsernameNotFoundException("No such user: " + name);
         }
 
         return new org.springframework.security.core.userdetails.User(
-                username,
-                this.accountDetails.get(username),
+                name,
+                this.accountDetails.get(name),
                 true,
                 true,
                 true,
